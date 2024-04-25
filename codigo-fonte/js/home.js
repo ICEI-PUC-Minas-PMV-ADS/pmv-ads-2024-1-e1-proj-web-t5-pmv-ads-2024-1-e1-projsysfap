15c29ecd6e86663fae7e5563e4a83cd1b36c9479
Erycson Jose da Silva <erycsonjose@agenciam3.com>
2024-04-25 19:23:58 -0300

    iniciar página home e outras páginas

---
+++ codigo-fonte/js/home.js
@@ -0,0 +1,11 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => document.getElementById('content').innerHTML = data)
+        .catch(error => console.error(error));
+}
+loadPage("dashboard.html");
+
+function logout(){
+    localStorage.removeItem("loggedUser");
+}
