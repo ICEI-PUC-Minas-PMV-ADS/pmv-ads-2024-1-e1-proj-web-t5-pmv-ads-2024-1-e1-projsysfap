b8fd6c431ea440348f94f8909b8a8f914d93b959
Gabriel <gabrielgama336@gmail.com>
2024-05-11 16:18:54 -0300

    Branch atualizada etapa 3 do projeto

--- codigo-fonte/js/home.js
+++ codigo-fonte/js/home.js
@@ -1,31 +1,30 @@
 function loadPage(page) {
     fetch(page)
-        .then(response => response.text())
-        .then(data => document.getElementById('content').innerHTML = data)
-        .catch(error => console.error(error));
-}
+.then(function (data) {
+  return data.text();
+})
+.then(function (html) {
+  document.getElementById('content').innerHTML = html;
+  var scripts = document.getElementById("content").querySelectorAll("script");
+  for (var i = 0; i < scripts.length; i++) {
+    if (scripts[i].innerText) {
+      eval(scripts[i].innerText);
+    } else {
+      fetch(scripts[i].src).then(function (data) {
+        data.text().then(function (r) {
+          eval(r);
+        })
+      });
+
+    }
+    // To not repeat the element
+    scripts[i].parentNode.removeChild(scripts[i]);
+  }
+});
+
+    }
 loadPage("dashboard.html");
 
 function logout(){
     localStorage.removeItem("loggedUser");
-}
-function activateNavButton(){
-    const btns = document.querySelectorAll(".nav .btn");
-    for (let btn of btns){
-        btn.addEventListener("click", function(){
-            for(let btn of btns){
-                btn.classList.remove("active");
-            }
-            this.classList.add("active");
-        })
-    }
-}
-window.onload = function(){
-    activateNavButton();
-    const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-    const userName = document.getElementById("userName");
-
-    if(loggedUser){
-        userName.innerHTML = loggedUser.user;
-    }    
 }
