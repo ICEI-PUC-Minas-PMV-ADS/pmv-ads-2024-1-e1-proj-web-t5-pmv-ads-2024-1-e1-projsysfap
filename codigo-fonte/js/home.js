2d7e51919aaad60373d80be077d0cb9d6cb4d497
pedropiresneves <166082773+pedropiresneves@users.noreply.github.com>
2024-05-11 23:19:24 -0300

    Update home.js

--- codigo-fonte/js/home.js
+++ codigo-fonte/js/home.js
@@ -1,7 +1,13 @@
 function loadPage(page) {
     fetch(page)
         .then(response => response.text())
-        .then(data => document.getElementById('content').innerHTML = data)
+        .then(data => {
+            const contentElement = document.getElementById("content");
+            contentElement.innerHTML = data;
+            if(page === "./orders.html"){
+                fetchOrders();
+            }
+        })
         .catch(error => console.error(error));
 }
 loadPage("dashboard.html");
@@ -28,4 +34,37 @@ window.onload = function(){
     if(loggedUser){
         userName.innerHTML = loggedUser.user;
     }    
+}
+
+function fetchOrders() {
+    fetch("../data/orderData.json")
+    .then(response => response.json())
+    .then(data => {
+        let newOrders = localStorage.getItem("orderData");
+        if(newOrders){
+            newOrders = JSON.parse(newOrders);
+
+            data = [...newOrders,...data];
+        }
+        let table = document.querySelector("#ordersTable tbody");
+        for(let item of data){
+            let date = new Date(item.orderDate);
+            let options = {year: "numeric", month: "long", day: "numeric"};
+            let formattedDate = date.toLocaleDateString("pt-BR", options);
+            let row = document.createElement("tr");
+            let time =  date.toLocaleTimeString();
+            row.innerHTML = `<td>${item.orderId}</td><td class"align-middle">${item.customerName}</td><td>${formattedDate} - ${time}</td><td>${formatAsReal(item.totalOrder)}</td>`;
+            table.appendChild(row);
+        }
+        showTotalOrders(data.length);
+    }).catch(error => console.error("Error", error));
+}
+function showTotalOrders(length){
+    const cardText = document.getElementById("ordersPage_totalOrders");
+    cardText.innerHTML = length.toString();
+}
+function formatAsReal(value) {
+    let numberWithTwoDecimalPlaces = value.toFixed(2);
+    let valueInReals = Number(numberWithTwoDecimalPlaces).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return valueInReals;
 }
