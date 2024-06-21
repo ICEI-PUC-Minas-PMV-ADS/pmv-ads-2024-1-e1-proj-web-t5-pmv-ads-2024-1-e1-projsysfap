a4fe91d4e8323a1254993259e3b9b0efba2aa55e
Heitor Oliveira <qbrocolis@gmail.com>
2024-06-21 08:17:02 -0300

    correções

--- codigo-fonte/js/home.js
+++ codigo-fonte/js/home.js
@@ -1,10 +1,51 @@
 function loadPage(page) {
     fetch(page)
         .then(response => response.text())
-        .then(data => document.getElementById('content').innerHTML = data)
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+            if (page === './customers.html'){
+                setupCustomerPage();
+            }
+        })
         .catch(error => console.error(error));
 }
+
 loadPage("dashboard.html");
+
+function setupCustomerPage() {
+    document.getElementById("viewCustomers").addEventListener("click", function () {
+        const users = loadUsersFromLocalStorage();
+        displayCustomers(users);
+    });
+    // Adiciona outros event listeners e configurações específicas da página de clientes aqui
+}
+
+function loadUsersFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("signupData")) || [];
+}
+
+function saveUsersToLocalStorage(users) {
+    localStorage.setItem("signupData", JSON.stringify(users));
+}
+
+function displayCustomers(data) {
+    const clienteTableBody = document.getElementById("clienteTableBody");
+    clienteTableBody.innerHTML = "";
+    data.forEach((cliente, index) => {
+        const row = `
+            <tr>
+                <td>${cliente.fullName}</td>
+                <td>${cliente.phoneNumber}</td>
+                <td>${cliente.email}</td>
+                <td>
+                    <button onclick="editCustomer(${index})">Editar</button>
+                    <button onclick="deleteCustomer(${index})">Excluir</button>
+                </td>
+            </tr>
+        `;
+        clienteTableBody.innerHTML += row;
+    });
+}
 
 function logout(){
     localStorage.removeItem("loggedUser");
@@ -27,5 +68,6 @@ window.onload = function(){
 
     if(loggedUser){
         userName.innerHTML = loggedUser.user;
-    }    
+    }
+    loadPage('dashboard.html');    
 }
